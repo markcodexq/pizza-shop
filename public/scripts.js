6266124c980a0786b841c9389f3f33153e5b2e45
markcodexq <markcodexq@gmail.com>
2022-05-16 20:00:05 +0500

    Pass id add to card

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,15 @@
-function something() 
-{
-    var x = window.localStorage.getItem('bbb'); // x == hh['bbb']
+// function something() 
+// {
+//     var x = window.localStorage.getItem('bbb'); // x == hh['bbb']
 
-    x = x * 1 + 1; // x = x + 1
+//     x = x * 1 + 1; // x = x + 1
 
-    window.localStorage.setItem('bbb', x); // hh['bbb'] = x
+//     window.localStorage.setItem('bbb', x); // hh['bbb'] = x
     
-    alert(x)
-}
+//     alert(x)
+// }
 
-function add_to_card()
+function add_to_card(id)
 {
-    alert("Hello from function!");
+    alert("Your added pizza with id " + id);
 }
