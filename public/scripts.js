ca39cb7164af03bb0a5914b6a908db28276dd3be
markcodexq <markcodexq@gmail.com>
2022-05-18 19:43:21 +0500

    Add cart items to localStorage

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
 // function something() 
 // {
-//     var x = window.localStorage.getItem('bbb'); // x == hh['bbb']
+//     var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
 //     x = x * 1 + 1; // x = x + 1
 
@@ -15,5 +15,25 @@ function add_to_card(id)
     
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
-    window.localStorage.setItem(key, x); 
+    window.localStorage.setItem(key, x);
+
+    alert("Items in your card: " + cart_get_number_of_items())
+}
+
+function cart_get_number_of_items()
+{
+    var cnt = 0;
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); // get key
+        var value = window.localStorage.getItem(key); // get value
+        
+        if(key.indexOf('product_') == 0)
+        {
+            cnt = cnt + value * 1;
+        }
+    }
+    
+    return cnt;
+
 }
