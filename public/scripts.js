fc079311b3218884ec6aace785ca430dc3333320
markcodexq <markcodexq@gmail.com>
2022-05-19 11:39:37 +0500

    Add cart_get_orders

--- public/scripts.js
+++ public/scripts.js
@@ -38,3 +38,22 @@ function cart_get_number_of_items()
     return cnt;
 
 }
+
+function cart_get_orders()
+{
+    var orders = "";
+    
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); // get key
+        var value = window.localStorage.getItem(key); // get value
+        
+        if(key.indexOf('product_') == 0)
+        {
+            orders = orders + key + "=" + value + ",";
+        }
+    }
+    
+    return orders;
+
+}
